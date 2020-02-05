d102f380299bda64069b904b260be8c1e056a0cf
Michal Butkiewicz <michal-butkiewicz@outlook.com>
2020-02-05 09:03:41 +0100

    Add port outputs control functions

--- main.c
+++ main.c
@@ -13,18 +13,27 @@
 #define SEG0 PA1
 #define SEG1 PA2
 #define REL PA5
-#define LED3 PB6 /* BLUE - pump work detected */
-#define LED2 PB5 /* RED - c.h. controller not working */
+#define LED3_BLUE PB6 /* BLUE - pump work detected */
+#define LED2_RED PB5 /* RED - c.h. controller not working */
 #define BCD_A PB0
 #define BCD_B PB1
 #define BCD_C PB2
 #define BCD_D PB3
 
-/* add 8MHz clock */
-
 void portInit(void);
 void adcInit(void);
 void timerInit(void);
+
+void segment0_on(void);
+void segment0_off(void);
+void segment1_on(void);
+void segment1_off(void);
+void relay_on(void);
+void relay_off(void);
+void led3Blue_on(void);
+void led3Blue_off(void);
+void led2Red_on(void);
+void led2Red_off(void);
 
 
 int main(void)
@@ -42,9 +51,20 @@ void portInit(void){
 	PORTA = (1<<SEG0) | (1<<SEG1) | (1<<REL);
 	DDRA =  (0<<TEMP) | (0<<CHCK) | (1<<SEG0) | (1<<SEG1) | (1<<REL);
 	
-	PORTB = (1<<LED3) | (1<<LED2) | (0<<BCD_D) | (0<<BCD_C) | (0<<BCD_B) | (0<<BCD_A);
-	DDRB = (1<<LED3) | (1<<LED2) | (1<<BCD_D) | (1<<BCD_C) | (1<<BCD_B) | (1<<BCD_A);
+	PORTB = (1<<LED3_BLUE) | (1<<LED2_RED) | (0<<BCD_D) | (0<<BCD_C) | (0<<BCD_B) | (0<<BCD_A);
+	DDRB = (1<<LED3_BLUE) | (1<<LED2_RED) | (1<<BCD_D) | (1<<BCD_C) | (1<<BCD_B) | (1<<BCD_A);
 }
+
+void segment0_on(void) {PORTA |= (0<<SEG0);}
+void segment0_off(void) {PORTA |= (1<<SEG0);}
+void segment1_on(void) {PORTA |= (0<<SEG1);}
+void segment1_off(void) {PORTA |= (1<<SEG1);}
+void relay_on(void) {PORTA |= (0<<REL);}
+void relay_off(void) {PORTA |= (1<<REL);}
+void led3Blue_on(void) {PORTB |= (0<<LED3_BLUE);}
+void led3Blue_off(void) {PORTB |= (1<<LED3_BLUE);}
+void led2Red_on(void) {PORTB |= (0<<LED2_RED);}
+void led2Red_off(void) {PORTB |= (1<<LED2_RED);}
 
 void adcInit(void){
 	
@@ -72,6 +92,9 @@ uint16_t covertAdcToTemp(uint16_t adc) {
 	*/
 	return (0.25*adc + 2);
 }
+
+
+
 
 void timerInit(void){
 	/* 7seg toggle time - 50ms */
